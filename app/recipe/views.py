e62c0533673a69cd8adfdf82bd50ac2798ed4e8f
legitk8 <aryamikpandey@gmail.com>
2024-09-07 00:59:07 +0530

    add update Tag API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -39,7 +39,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """View to manage Tag APIs."""
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
