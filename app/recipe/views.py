48f60ad3824b37eed209efac756963273ce8a214
legitk8 <aryamikpandey@gmail.com>
2024-09-06 15:05:34 +0530

    override serializer_class and create

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,12 +6,12 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Recipe
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View to manage Recipe APIs."""
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -19,3 +19,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipe for the authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return serializer class for the request."""
+        if self.action == 'list':
+            return RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe with the authenticated user."""
+        serializer.save(user=self.request.user)
